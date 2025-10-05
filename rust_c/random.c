62af286dc21a3281bc6901d07279fc58def87076
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2025-10-05 18:30:55 -0300

    função em C está gerando os números aleatórios para o Rust

--- rust_c/random.c
+++ rust_c/random.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <time.h>
+#include <time.h>
 
-void abi_teste(){
-    printf("comando feito em C!\n");
+void init_random() {
+    srand((unsigned)time(NULL));
+}
+
+int random(int num){
+    return rand() % num + 1;
 }
--- rust_c/src/main.rs
+++ rust_c/src/main.rs
@@ -1,10 +1,13 @@
 unsafe extern "C" {
-    fn abi_teste();
+    fn init_random();
+    fn random(num: i32) -> i32;
 }
 
 fn main() {
     println!("Hello, world!");
-    unsafe {
-        abi_teste();
-    }
+
+    unsafe { init_random(); }
+    let teste = unsafe { random(10) };
+    println!("{teste}");
+    
 }

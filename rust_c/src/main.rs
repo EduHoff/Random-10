496a34777fc31c2937e17e911bcfaac1808db067
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2025-10-06 10:43:03 -0300

    função para definir os 10 números aleatórios

--- rust_c/src/main.rs
+++ rust_c/src/main.rs
@@ -3,11 +3,20 @@ unsafe extern "C" {
     fn random(num: i32) -> i32;
 }
 
+fn generate_list(list_random: &mut [i32; 10]){
+    unsafe { init_random(); }
+    for i in 0..list_random.len() {
+        list_random[i] = unsafe { random(6) };
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+  
+    let mut list_random: [i32; 10] = [0; 10];
+    generate_list(&mut list_random);
 
-    unsafe { init_random(); }
-    let teste = unsafe { random(10) };
-    println!("{teste}");
-    
+    for aux in list_random{
+        println!("{aux}");
+    }
 }
